bb052c06a195e6d558f0be7f2a8d600db73b148e
hassan-alansari <66649286+hassan-alansari@users.noreply.github.com>
2020-06-27 21:33:20 +0300

    g

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,32 @@
 // السؤال الأول
 // 1
-
+const name = "hassan";
 // 2
-
+var age = "16";
 // 3 and 4
-
+console.log("انا" + name + "و عمري" + age);
 // 5 and 6
-
+console.log(age * 5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobies = ["listening story", "running", "waching", 16];
 // 2
-
+hobies.push = 16;
 // 3
-
+hobies.pop(age);
 // 4
-
+var weblang = {
+  ta: ["نانسي", "حسين", "رهف", "موضي", "سمو", "خاجه"],
+  web: "font-end-dev",
+  mylang: "css",
+};
 // 5
-
 // 6
-
+console.log(weblang.web);
 // 7
-
+console.log(weblang.ta[3]);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
